ed584ae71e6086dbfe3e9dcf2a79d15a0836ac5f
YaazKYP <kanyapat.yayee@gmail.com>
2026-09-24 12:38:02 +0100

    Conditon

--- conditionTest.js
+++ conditionTest.js
@@ -1,2 +1,19 @@
 // file condition
-let
+
+
+let playerScore = 0;
+
+function addPlayerScore() {
+  playerScore += 10;
+}
+
+function checkOddOrEven() {
+  if (playerScore % 2 === 0) {
+    console.log("Score is " + playerScore + ": EVEN number!");
+  } else {
+    console.log("Score is " + playerScore + ": ODD number!");
+  }
+}
+
+addPlayerScore();
+checkOddOrEven();
